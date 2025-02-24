b4bce1a1e472436124e839456a9980867a291b09
Luis Eduardo L. Gisoldi <eduardogisoldi@gmail.com>
2025-02-23 21:31:19 -0300

    Update a04_pilha.h

--- USCS - 2024.2/Algoritmos e Estrutura de Dados 3/Tarefas/a04_pilha.h
+++ USCS - 2024.2/Algoritmos e Estrutura de Dados 3/Tarefas/a04_pilha.h
@@ -1,27 +1,42 @@
-//Definiçao do elemento da Pilha
+#include <stdio.h>
+/*
+int x = 1;
+int* pX = &x; // 1321111
+
+pX // 1321111
+*pX // 1
+
+
+Pilha x
+topo([10] ->) [7] -> [5] ->
+tam 3
+
+*/
+
+
+
+//Definicao do elemento da Pilha
 struct No{
 	int valor;
 	struct No* prox;
 };
-
 typedef struct No Elemento;
 
-//Definição da Pilha
+//Definicao da Pilha
 struct P {
 	Elemento* topo;
 	int tam;
 };
-
 typedef struct P Pilha;
 
 //Cria uma pilha vazia
 Pilha* cria_pilha();
 
-//Exibe os elementos da pilha do topo até o final
+//Exibe os elementos da pilha do topo atï¿½ o final
 // da pilha
 void exibe (Pilha*);
 
-//Devolve 1 se a pilha é vazia e 0, caso contrário
+//Devolve 1 se a pilha ï¿½ vazia e 0, caso contrï¿½rio
 int Pilha_vazia (Pilha*);
 
 //Insere um elemento no topo da pilha
@@ -30,7 +45,7 @@ void empilha (Pilha*, int);
 //Remove o elemento do topo da pilha
 Elemento* desempilha (Pilha*);
 
-//Devolve o elemento que está no topo da pilha
+//Devolve o elemento que estï¿½ no topo da pilha
 Elemento* topo (Pilha*);
 
 //Inverte os elementos da pilha
